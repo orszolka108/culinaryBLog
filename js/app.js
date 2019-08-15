9b1bcbc7564f1802bdc6423fa9f80b5202613bef
Urszula Duda <orszolka108@gmail.com>
2019-08-15 20:25:47 +0200

    Html and Css refactor, mobile layout

--- js/app.js
+++ js/app.js
@@ -1,30 +1,10 @@
-//status bar
-document.addEventListener("DOMContentLoaded", function () {
-    const status = document.querySelector('.status');
-    const statusBar = () => {
-        currentScroll = Math.round(window.scrollY / (document.documentElement.offsetHeight - window.innerHeight) * 100);
 
-        status.style.width = currentScroll + "%";
-    }
 
-    window.addEventListener("scroll", statusBar);
-
-// recipes
-    const recipe = document.querySelector('.recipe-box');
-    const recipeBtn = document.querySelector('.recipe-button');
-    const closeBtn = document.querySelector('.close-button');
-
-    recipeBtn.addEventListener('click', function(){
-        recipe.classList.toggle('show-recipe');
-    })
-
-    closeBtn.addEventListener('click', function() {
-        recipe.classList.toggle('show-recipe');
-    })
-
+$('.menu').on('click', function() {
+    $(this).toggleClass('active');
+    $('.overlay').toggleClass('menu-open')
+   
 });
-
-
 
 
 
