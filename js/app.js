c1b588304871eeeec5f9055ed92837170c42c7d7
Urszula Duda <orszolka108@gmail.com>
2019-09-03 15:49:55 +0200

    Hover w galerii >762px

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
+$(document).ready(function(){
+    $('.menu').on('click', function() {
+        $(this).toggleClass('active');
+        $('.overlay').toggleClass('menu-open')
+       
+    });
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
 
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 
-$('.menu').on('click', function() {
-    $(this).toggleClass('active');
-    $('.overlay').toggleClass('menu-open')
-   
 });
 
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const status = document.querySelector('.status');
+//     const statusBar = () => {
+//         currentScroll = Math.round(window.scrollY / (document.documentElement.offsetHeight - window.innerHeight) * 100);
+
+//         status.style.width = currentScroll + "%";
+//     }
+
+//     window.addEventListener("scroll", statusBar);
+
+// }
 
 
 
